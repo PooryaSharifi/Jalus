824ed4b5e16cd0d6cce0633f75490d897d308e18
Arsha <po00oryasharifi@gmail.com>
2024-11-12 01:21:47 -0800

    debabel 2nd try

--- jalus.py
+++ jalus.py
@@ -3,7 +3,7 @@ from sanic import Sanic, Blueprint, response, exceptions
 from random import choice, randint, random
 from bson import ObjectId
 from datetime import datetime, timedelta
-from static import load_template, render_template, wild_origins, wild_filters, decode, encode, template_variables
+from static import load_template, render_template, wild_origins, wild_filters, decode, encode, template_titles
 from io import BytesIO, StringIO
 
 db_uri, db_name = "mongodb://{host}:{port}/".format(host="localhost", port=27017), os.path.basename(os.path.dirname(__file__)).capitalize()
@@ -153,7 +153,7 @@ async def _smart_home_state(r, home, ):
 @app.get('/homes/<home>')
 async def _smart_home(r, home, ): return response.html((await response.file(f"{os.path.dirname(os.path.abspath(__file__))}/templates/Home{'' if '-d' in sys.argv or '--debug' in sys.argv else '.serv'}.html")).body.decode('utf-8')) 
 @app.get('/<page:path>')
-async def _page(r, page=None): page = 'jalus' if page == '' else page; return response.html((await render_template('base.html', template_variables)).replace('/// block #content', await render_template(f'{page.capitalize()}.js', {})) if '-d' in sys.argv else await load_template(f'serv/{page.capitalize()}.html'))
+async def _page(r, page=None): page = 'jalus' if page == '' else page; return response.html((await render_template('base.html', {'title': template_titles[page], 'style': 'digikala'})).replace('/// block #content', await render_template(f'{page.capitalize()}.js', {})) if '-d' in sys.argv else await load_template(f'serv/{page.capitalize()}.html'))
 
 if __name__ == '__main__':
     debug = True if '-d' in sys.argv or '--debug' in sys.argv else False
